46bae83cae9307275ecda511be0faab24aa6fe45
UC-GitHub <yasutsugu58@gmail.com>
2020-09-06 14:39:03 +0900

    usersコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  # VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  # VALID_KANANAME_REGEX = /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/
+
+  # validates : nickname, presence: true,
+  # validates : email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  # validates : encrypted_password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  # validates : first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  # validates : family_name, presence: true, format: { with: VALID_NAME_REGEX }
+  # validates : kana_first_name, presence: true, format: { with: VALID_KANANAME_REGEX }
+  # validates : kana_family_name, presence: true, format: { with: VALID_KANANAME_REGEX }
+  # validates : birthday, presence: true
+
+  has_many :items
+  has_many :purchases
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'items/index'
   root to: "items#index"
+  devise_for :users
 end
 
+
+# ChatAppのルーティング↓
+# Rails.application.routes.draw do
+#   devise_for :users
+#   get 'messages/index'
+#   root "rooms#index"
+#   resources :users, only: [:edit, :update]
+#   resources :rooms, only: [:new, :create, :destroy] do
+#     resources :messages, only: [:index, :create]
+#   end
+# end
