027076e5f0b9fe04998346669901543edcba7527
UC-GitHub <yasutsugu58@gmail.com>
2020-09-06 21:08:44 +0900

    新規登録バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  # VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  # VALID_KANANAME_REGEX = /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_KANANAME_REGEX = /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/
 
-  # validates : nickname, presence: true,
-  # validates : email, presence: true, format: { with: VALID_EMAIL_REGEX }
-  # validates : encrypted_password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
-  # validates : first_name, presence: true, format: { with: VALID_NAME_REGEX }
-  # validates : family_name, presence: true, format: { with: VALID_NAME_REGEX }
-  # validates : kana_first_name, presence: true, format: { with: VALID_KANANAME_REGEX }
-  # validates : kana_family_name, presence: true, format: { with: VALID_KANANAME_REGEX }
-  # validates : birthday, presence: true
-
+  validates :nickname, presence: true
+  # validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'must include both letters and numbers'  }
+  devise :validatable, password_length: 6..128
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'must be Full-width characters' }
+  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'must be Full-width characters' }
+  validates :kana_first_name, presence: true, format: { with: VALID_KANANAME_REGEX, message: 'must be Full-width katakana characters' }
+  validates :kana_family_name, presence: true, format: { with: VALID_KANANAME_REGEX, message: 'must be Full-width katakana characters' }
+  validates :birthday, presence: true
+  
   has_many :items
   has_many :purchases
 
