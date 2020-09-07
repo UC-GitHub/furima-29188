4ab4bf5a78aa456dba08cc39568f8bea4ae8ac8e
UC-GitHub <yasutsugu58@gmail.com>
2020-09-07 21:52:01 +0900

    Active Hush追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :shipping_from
+  belongs_to_active_hash :shipping_date
+
+  has_one :purchase
+  belongs_to :user
+
+  # *追記 validates :image, :text, :category, presence: true
+  validates :category_id, :status_id, :shipping_fee_id, :shipping_date_id, numericality: { other_than: 1 } 
+  validates :shipping_from_id, numericality: { other_than: 0 }
+
+
 end
--- db/migrate/20200907101821_create_items.rb
+++ db/migrate/20200907101821_create_items.rb
@@ -1,7 +1,6 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :image,              null: false
       t.string :name,               null: false
       t.string :description,        null: false
       t.integer :category_id,       null: false
@@ -10,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :shipping_from_id,  stnull: false
       t.integer :shipping_date_id,  null: false
       t.integer :price,             null: false
-      t.references :user,           null: false
+      t.references :user,           foreign_key: true
       t.timestamps
     end
   end
