00f968e9bca44948b9fcf87a5410176ccac484bb
UC-GitHub <yasutsugu58@gmail.com>
2020-09-07 17:42:28 +0900

    不要なコメントアウトの削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,3 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
 end
-
-
-# ChatAppのルーティング↓
-# Rails.application.routes.draw do
-#   devise_for :users
-#   get 'messages/index'
-#   root "rooms#index"
-#   resources :users, only: [:edit, :update]
-#   resources :rooms, only: [:new, :create, :destroy] do
-#     resources :messages, only: [:index, :create]
-#   end
-# end
