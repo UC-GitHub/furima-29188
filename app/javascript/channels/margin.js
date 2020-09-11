4ec73235d9476e7f23e355d9755ff524410bb688
UC-GitHub <yasutsugu58@gmail.com>
2020-09-11 16:49:50 +0900

    プルリクエストの修正3

--- app/javascript/channels/margin.js
+++ app/javascript/channels/margin.js
@@ -1,20 +1,12 @@
 window.addEventListener("load", calculation);
 
-function calculation() {
-  const item_price = document.getElementById("item-price");
-  let margin = document.getElementById("add-tax-price");
-  let profit = document.getElementById("profit");
-  console.log(item_price)
-  item_price.addEventListener("input", () => {
-  const price = document.getElementById("item-price").value;
-  console.log(price)
-  margin.innerHTML = Math.floor(price * 0.1);
-  profit.innerHTML = (price - margin.innerHTML);
-  console.log(margin)
-  console.log(profit)
-});
-}
-
-
-
-{/* <input type="text" onInput="alertValue(this)" /> */}
+  function calculation() {
+    const item_price = document.getElementById("item-price");
+    let margin = document.getElementById("add-tax-price");
+    let profit = document.getElementById("profit");
+      item_price.addEventListener("input", () => {
+        const price = document.getElementById("item-price").value;
+          margin.innerHTML = Math.floor(price * 0.1);
+          profit.innerHTML = (price - margin.innerHTML);
+  });
+  }
