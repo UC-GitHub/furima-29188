dea19f4e2ea688aa0fcf16cd8a8a2401a523440a
UC-GitHub <yasutsugu58@gmail.com>
2020-09-16 15:35:40 +0900

    クレジットAPI設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,30 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def create
+      @item = Item.find(params[:item_id])
+      @order = Order.new(order_params)
+      if @order.valid?
+        pay_item
+        @order.save
+        return redirect_to root_path
+      else
+        # render :index
+      end
+  end
+
   private
   def order_params
-    params.require(:order).merge(user_id: current_user.id, item_id: parrams[:item_id])
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id]) 
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'
+    )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :shipping_address
+  
+  attr_accessor :price
+  attr_accessor :token
+  validates :price, presence: true
+  validates :token, presence: true
 end
